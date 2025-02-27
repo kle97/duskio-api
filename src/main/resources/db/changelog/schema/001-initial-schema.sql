0fc1d1f86f16f0fd08f0769f211d18ac65fb78ad
Khoa Le <khoale1308@gmail.com>
2025-02-26 22:36:52 -0800

    Move to MySQL

--- src/main/resources/db/changelog/schema/001-initial-schema.sql
+++ src/main/resources/db/changelog/schema/001-initial-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE author
 (
-    author_id        INT GENERATED ALWAYS AS IDENTITY,
+    author_id        INT          NOT NULL AUTO_INCREMENT,
     author_name      VARCHAR(512) NOT NULL,
     birth_date       DATE,
     death_date       DATE,
@@ -9,9 +9,9 @@ CREATE TABLE author
     photo            VARCHAR(255),
     ol_key           VARCHAR(255),
     created_by       VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by VARCHAR(255) NOT NULL,
-    last_modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision         INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (author_id)
 );
@@ -19,13 +19,13 @@ CREATE TABLE author
 
 CREATE TABLE alternate_name
 (
-    alternate_name_id INT GENERATED ALWAYS AS IDENTITY,
+    alternate_name_id INT          NOT NULL AUTO_INCREMENT,
     alternate_name    VARCHAR(512) NOT NULL,
     author_id         INT          NOT NULL,
     created_by        VARCHAR(255) NOT NULL,
-    created_at        TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by  VARCHAR(255) NOT NULL,
-    last_modified_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision          INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (alternate_name_id),
     FOREIGN KEY (author_id) REFERENCES author (author_id) ON DELETE CASCADE
@@ -34,14 +34,14 @@ CREATE TABLE alternate_name
 
 CREATE TABLE author_link
 (
-    author_link_id   INT GENERATED ALWAYS AS IDENTITY,
+    author_link_id   INT          NOT NULL AUTO_INCREMENT,
     title            VARCHAR(512),
     url              VARCHAR(512),
     author_id        INT          NOT NULL,
     created_by       VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by VARCHAR(255) NOT NULL,
-    last_modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision         INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (author_link_id),
     FOREIGN KEY (author_id) REFERENCES author (author_id) ON DELETE CASCADE
@@ -50,13 +50,13 @@ CREATE TABLE author_link
 
 CREATE TABLE work
 (
-    work_id          INT GENERATED ALWAYS AS IDENTITY,
+    work_id          INT          NOT NULL AUTO_INCREMENT,
     title            VARCHAR(512) NOT NULL,
     ol_key           VARCHAR(255),
     created_by       VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by VARCHAR(255) NOT NULL,
-    last_modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision         INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (work_id)
 );
@@ -74,12 +74,12 @@ CREATE TABLE author_work
 
 CREATE TABLE subject
 (
-    subject_id       INT GENERATED ALWAYS AS IDENTITY,
+    subject_id       INT          NOT NULL AUTO_INCREMENT,
     subject_name     VARCHAR(512) NOT NULL,
     created_by       VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by VARCHAR(255) NOT NULL,
-    last_modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision         INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (subject_id)
 );
@@ -97,27 +97,27 @@ CREATE TABLE work_subject
 
 CREATE TABLE rating
 (
-    work_id          INT GENERATED ALWAYS AS IDENTITY,
+    work_id          INT          NOT NULL AUTO_INCREMENT,
     score            INT          NOT NULL,
     rating_id        INT          NOT NULL,
     created_by       VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by VARCHAR(255) NOT NULL,
-    last_modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision         INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (rating_id),
-    FOREIGN KEY (work_id) REFERENCES work (work_id) ON DELETE CASCADE,
+    FOREIGN KEY (work_id) REFERENCES work (work_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE publisher
 (
-    publisher_id     INT GENERATED ALWAYS AS IDENTITY,
+    publisher_id     INT          NOT NULL AUTO_INCREMENT,
     publisher_name   VARCHAR(255) NOT NULL,
     created_by       VARCHAR(255) NOT NULL,
-    created_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by VARCHAR(255) NOT NULL,
-    last_modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision         INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (publisher_id)
 );
@@ -125,7 +125,7 @@ CREATE TABLE publisher
 
 CREATE TABLE edition
 (
-    edition_id          INT GENERATED ALWAYS AS IDENTITY,
+    edition_id          INT          NOT NULL AUTO_INCREMENT,
     title               VARCHAR(512) NOT NULL,
     subtitle            VARCHAR(512),
     description         VARCHAR(8192),
@@ -151,12 +151,11 @@ CREATE TABLE edition
     publisher_id        INT,
     work_id             INT,
     created_by          VARCHAR(255) NOT NULL,
-    created_at          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at          DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_modified_by    VARCHAR(255) NOT NULL,
-    last_modified_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_at    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revision            INT          NOT NULL DEFAULT 1,
     PRIMARY KEY (edition_id),
     FOREIGN KEY (work_id) REFERENCES work (work_id) ON DELETE SET NULL,
     FOREIGN KEY (publisher_id) REFERENCES publisher (publisher_id) ON DELETE SET NULL
 );
-
